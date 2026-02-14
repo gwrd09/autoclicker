e184c03e301c1254e33a79586eb0aebc11a7e620
George Ward <gwardgaming@gmail.com>
2026-02-14 19:21:03 +0000

    Added a simple GUI with the ability to set the amount of times it clicks

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
+from PySide6 import QtWidgets
 import pyautogui
+
+class AutoClickerGUI(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.amount_input = QtWidgets.QSpinBox()
+        self.start_button = QtWidgets.QPushButton("Start Autoclicker")
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.amount_input)
+        self.layout.addWidget(self.start_button)
+
+        self.start_button.clicked.connect(self.start_autoclicker)
+
+    def start_autoclicker(self):
+        amount = self.amount_input.value()
+        auto_left_click(amount)
 
 def auto_left_click(amount=None):
     if amount:
@@ -6,3 +24,13 @@ def auto_left_click(amount=None):
             pyautogui.leftClick()
     else:
         pyautogui.leftClick()
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication()
+
+    widget = AutoClickerGUI()
+    widget.setWindowTitle("Autoclicker!")
+    widget.setFixedSize(300, 350)
+    widget.show()
+
+    app.exec()
