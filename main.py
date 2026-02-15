204437d2629cefd31f81d8eec3e15601597d0752
George Ward <gwardgaming@gmail.com>
2026-02-15 19:58:15 +0000

    Refactored the auto_left_click function and reimagined the inheritance of the main GUI along with the layout and containers

--- main.py
+++ main.py
@@ -1,36 +1,37 @@
 from PySide6 import QtWidgets
 import pyautogui
 
-class AutoClickerGUI(QtWidgets.QWidget):
+class AutoClickerGUI(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("Autoclicker!")
+        self.setFixedSize(300, 350)
         self.amount_input = QtWidgets.QSpinBox()
         self.start_button = QtWidgets.QPushButton("Start Autoclicker")
 
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.amount_input)
-        self.layout.addWidget(self.start_button)
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.amount_input)
+        layout.addWidget(self.start_button)
 
-        self.start_button.clicked.connect(self.start_autoclicker)
+        container = QtWidgets.QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
 
-    def start_autoclicker(self):
+        self.start_button.clicked.connect(self.auto_left_click)
+
+    def auto_left_click(self):
         amount = self.amount_input.value()
-        auto_left_click(amount)
-
-def auto_left_click(amount):
-    if amount == 0:
-        while True:
-            pyautogui.leftClick()
-    else:
-        for i in range(amount):
-            pyautogui.leftClick()
+        if amount == 0:
+            while True:
+                pyautogui.click()
+        else:
+            for i in range(amount):
+                pyautogui.click()
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication()
 
     widget = AutoClickerGUI()
-    widget.setWindowTitle("Autoclicker!")
-    widget.setFixedSize(300, 350)
     widget.show()
 
     app.exec()
