cc8321112c87ed91569d2ca6a573a7c00c1dea1f
George Ward <gwardgaming@gmail.com>
2026-02-15 20:37:01 +0000

    Changed the way to make the autoclicker continue infinitely from 0 to -1, and changed how the window sizes

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ class AutoClickerGUI(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Autoclicker!")
-        self.setFixedSize(300, 350)
+        self.setFixedWidth(250)
+        self.adjustSize()
+        self.setFixedSize(self.size())
+        self.amount_label = QtWidgets.QLabel("Amount:")
         self.amount_input = QtWidgets.QSpinBox()
+        self.amount_input.setRange(-1, 999999)
         self.start_button = QtWidgets.QPushButton("Start Autoclicker")
 
         layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.amount_label)
         layout.addWidget(self.amount_input)
         layout.addWidget(self.start_button)
 
@@ -21,11 +26,13 @@ class AutoClickerGUI(QtWidgets.QMainWindow):
 
     def auto_left_click(self):
         amount = self.amount_input.value()
-        if amount == 0:
+        if amount == -1:
             while True:
                 pyautogui.click()
+        if amount == 0:
+            pass
         else:
-            for i in range(amount):
+            for _ in range(amount):
                 pyautogui.click()
 
 if __name__ == "__main__":
