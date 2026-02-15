18fbfc8985ad6bc7bab62954a88539a28b783477
George Ward <gwardgaming@gmail.com>
2026-02-15 21:09:38 +0000

    Added the ability to choose the clicks per second

--- main.py
+++ main.py
@@ -11,16 +11,20 @@ class AutoClickerGUI(QtWidgets.QMainWindow):
         super().__init__()
         self.setWindowTitle("Autoclicker!")
         self.setFixedWidth(250)
-        self.adjustSize()
-        self.setFixedSize(self.size())
+        self.setFixedHeight(150 )
         self.amount_label = QtWidgets.QLabel("Amount:")
         self.amount_input = QtWidgets.QSpinBox()
         self.amount_input.setRange(-1, 999999)
+        self.cps_label = QtWidgets.QLabel("Clicks Per Second:")
+        self.cps_input = QtWidgets.QSpinBox()
+        self.cps_input.setRange(1, 30)
         self.start_button = QtWidgets.QPushButton("Start Autoclicker")
 
         layout = QtWidgets.QVBoxLayout()
         layout.addWidget(self.amount_label)
         layout.addWidget(self.amount_input)
+        layout.addWidget(self.cps_label)
+        layout.addWidget(self.cps_input)
         layout.addWidget(self.start_button)
 
         container = QtWidgets.QWidget()
@@ -31,16 +35,17 @@ class AutoClickerGUI(QtWidgets.QMainWindow):
 
     def auto_left_click(self):
         amount = self.amount_input.value()
+        cps = self.cps_input.value()
         if amount == -1:
             while True:
                 pyautogui.click()
-                time.sleep(1/20)
+                time.sleep(1/cps)
         if amount == 0:
             pass
         else:
             for _ in range(amount):
                 pyautogui.click()
-                time.sleep(1/20)
+                time.sleep(1/cps)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication()
